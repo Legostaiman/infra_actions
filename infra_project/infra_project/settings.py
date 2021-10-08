cc88bb547a98567d2b16c046fe166ba8a56245aa
Aliaksei Liahostseu <legostaiman@mail.ru>
2021-10-09 01:52:49 +0200

    deploy job v7

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '178.154.229.211']
 
 # Application definition
 
